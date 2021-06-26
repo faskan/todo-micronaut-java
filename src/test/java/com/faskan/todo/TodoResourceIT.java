5040aa8af75edcd5bc1840c7c51a8f67032d4c1a
faskan <faisalkhanthayub@gmail.com>
2021-06-27 00:24:20 +0200

    run testcontainer as static

--- src/test/java/com/faskan/todo/TodoResourceIT.java
+++ src/test/java/com/faskan/todo/TodoResourceIT.java
@@ -1,43 +1,44 @@
 package com.faskan.todo;
 
-import io.micronaut.context.ApplicationContext;
-import io.micronaut.context.env.PropertySource;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.json.JSONException;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Testcontainers
+//@Testcontainers
+@MicronautTest
 public class TodoResourceIT {
 
-    @Container
+    //@Container
     private static MongoDBContainer mongoDBContainer =
             new MongoDBContainer(DockerImageName.parse("mongo:4.2"));
-
     private static EmbeddedServer embeddedServer;
     private static HttpClient client;
 
-    @BeforeAll
-    public static void init() {
-        embeddedServer = ApplicationContext.run(EmbeddedServer.class, PropertySource.of(
-                "test", Map.of("mongodb.uri", mongoDBContainer.getReplicaSetUrl("micronaut"))
-        ));
-        client = embeddedServer.getApplicationContext().getBean(HttpClient.class);
+
+    static {
+        mongoDBContainer.start();
+        System.setProperty("mongodb.uri",
+                mongoDBContainer.getReplicaSetUrl("micronaut"));
     }
+
+    //    @BeforeAll - an alternative
+//    public static void init() {
+//        embeddedServer = ApplicationContext.run(EmbeddedServer.class, PropertySource.of(
+//                "test", Map.of("mongodb.uri", mongoDBContainer.getReplicaSetUrl("micronaut"))
+//        ));
+//        client = embeddedServer.getApplicationContext().getBean(HttpClient.class);
+//    }
 
 
     @Test
